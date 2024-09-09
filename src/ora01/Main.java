92e5ca1c76d9dfd2f9c1214b2feb37eb5c950a1f
Dan <v0daniel01@gmail.com>
2024-09-09 13:14:05 +0200

    May the force be with you.

--- src/ora01/Main.java
+++ src/ora01/Main.java
@@ -3,7 +3,7 @@ package ora01;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("körte");
+		System.out.println("Master Skywalker there are too many of 'em.. what are we going to do?");
 		
 	}
 
